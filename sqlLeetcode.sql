cd62fb367a164e67841c2fc0f3b5936b7ab650b4
Somnath Jadhav <jadhavsomnath7005@gmail.com>
2025-09-01 21:53:47 +0530

    ---

--- sqlLeetcode.sql
+++ sqlLeetcode.sql
@@ -1,7 +1,7 @@
-1757 . Recyclable and Low Fat Products
+-- 1757 . Recyclable and Low Fat Products
     	SELECT product_id FROM Products WHERE low_fats = 'Y' AND recyclable = 'Y';
 
-584. Find Customer Referee
+-- 584. Find Customer Referee
      	SELECT name
     	FROM Customer
  	WHERE referee_id IS NULL OR referee_id != 2;
