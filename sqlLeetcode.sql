0802839a57754b9b8a71493b740fdb42a33ab6b5
Somnath Jadhav <jadhavsomnath7005@gmail.com>
2025-09-09 21:01:39 +0530

    '...

--- sqlLeetcode.sql
+++ sqlLeetcode.sql
@@ -51,3 +51,12 @@
     JOIN Weather Y
     ON T.recordDate = Y.recordDate + INTERVAL '1 day'
     WHERE T.temperature > Y.temperature;
+
+-- 1661. Average Time of Process per Machine
+   SELECT a1.machine_id, ROUND(AVG(a2.timestamp - a1.timestamp)::numeric, 3) AS               processing_time
+   FROM Activity a1
+   JOIN Activity a2 
+   ON a1.machine_id = a2.machine_id
+   WHERE a1.activity_type = 'start' AND a2.activity_type = 'end'
+   GROUP BY a1.machine_id;
+
