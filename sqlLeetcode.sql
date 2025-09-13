a10736d00a16adf72c00773484c8fc67d585fef6
Somnath Jadhav <jadhavsomnath7005@gmail.com>
2025-09-13 06:46:12 +0530

    ----

--- sqlLeetcode.sql
+++ sqlLeetcode.sql
@@ -86,3 +86,12 @@
      FROM Employee 
      GROUP BY managerId 
      HAVING COUNT(*) >= 5)
+
+-- 1934. Confirmation Rate
+ SELECT s.user_id, 
+    ROUND(AVG(CASE WHEN c.action = 'confirmed' THEN 1 ELSE 0 END), 2) AS       confirmation_rate
+ FROM Signups s
+ LEFT JOIN Confirmations c
+ ON s.user_id = c.user_id
+ GROUP BY s.user_id;
+
