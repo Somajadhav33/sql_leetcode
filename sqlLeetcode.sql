eabd954bc149558c566af67b318380bff6998fb2
Somnath Jadhav <jadhavsomnath7005@gmail.com>
2025-09-18 20:13:26 +0530

    ===

--- sqlLeetcode.sql
+++ sqlLeetcode.sql
@@ -114,3 +114,10 @@
   FROM Project p LEFT JOIN Employee e
   ON p.employee_id = e.employee_id
   GROUP BY  p.project_id;
+
+-- 1211. Queries Quality and Percentage
+SELECT query_name, 
+    ROUND(AVG(rating * 1.0/position), 2) AS quality,
+    ROUND(AVG(CASE WHEN rating < 3 THEN 1 ELSE 0 END ) * 100, 2) AS poor_query_percentage
+FROM Queries 
+GROUP BY query_name
