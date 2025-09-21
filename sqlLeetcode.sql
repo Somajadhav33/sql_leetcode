bc7b4ad6b3580df71e56908dc8b286fae48fae90
Somnath Jadhav <jadhavsomnath7005@gmail.com>
2025-09-21 09:10:47 +0530

    ----

--- sqlLeetcode.sql
+++ sqlLeetcode.sql
@@ -138,3 +138,11 @@ FROM Delivery
 WHERE (customer_id, order_date) IN 
         (SELECT customer_id, MIN(order_date) FROM Delivery GROUP BY customer_id);
 
+-- 550. Game Play Analysis IV
+SELECT ROUND(1.0* COUNT(player_id)/ (SELECT COUNT(DISTINCT player_id) FROM Activity), 2) AS fraction
+FROM Activity
+WHERE (player_id , event_date) IN (
+    SELECT player_id, MIN(event_date) + 1
+    FROM Activity
+    GROUP BY player_id
+)
