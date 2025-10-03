42a7c06c39b57e0686d1684eeeaefac75a963560
Somnath Jadhav <jadhavsomnath7005@gmail.com>
2025-10-03 21:34:14 +0530

    ---

--- sqlLeetcode.sql
+++ sqlLeetcode.sql
@@ -223,3 +223,19 @@ SELECT x, y, z,
         THEN 'Yes' ELSE 'No'
     END AS triangle
 FROM Triangle 
+
+-- 1164. Product Price at a Given Date
+SELECT distinct product_id, 10 as price
+from Products
+group by product_id
+having min(change_date) > '2019-08-16'
+
+Union 
+
+select distinct product_id, new_price as price
+from Products
+where(product_id, change_date) in (
+    select product_id, max(change_date)
+    from Products where change_date <= '2019-08-16'
+    group by product_id
+)
