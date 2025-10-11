08c8b02566880c074b38588eb9c5270354ee7c30
Somnath Jadhav <jadhavsomnath7005@gmail.com>
2025-10-11 14:29:26 +0530

    ---

--- sqlLeetcode.sql
+++ sqlLeetcode.sql
@@ -334,3 +334,54 @@ SELECT accepter_id AS id FROM RequestAccepted)
 
 SELECT id, COUNT(id) AS num FROM temp GROUP BY id 
 ORDER BY num DESC LIMIT 1
+
+
+ #include <bits/stdc++.h>
+using namespace std;
+
+class solution{
+    public:
+    void mergeSort(int arr[], int n){
+        mergeSort(arr,0,n-1);
+    }
+    private:
+        void mergeSort(int arr[],int left,int right){
+            if(left==right) return;
+            int mid=(left+right)/2;
+            mergeSort(arr,left,mid);
+            mergeSort(arr,mid+1,right);
+            merge(arr,left,mid,right);
+        }
+        
+        void merge(int arr[],int left,int mid,int right){
+            int n=right-left+1;
+            int new_arr[n];
+            int i=left,j=mid+1,k=0;
+            
+            while(i<=mid && j<=right){
+                if(arr[i]<arr[j]){
+                    new_arr[k]=arr[i];
+                    i++;
+                    k++;
+                }else{
+                    new_arr[k]=arr[j];
+                    j++;
+                    k++;
+                }
+            }
+            while(i<=mid){
+                new_arr[k]=arr[i];
+                i++;
+                k++;
+            }
+            while(j<=right){
+                new_arr[k]=arr[j];
+                j++;
+                k++;
+            }
+            
+            for(int c=0;c<n;c++){
+                arr[left+c]=new_arr[c];
+            }
+        }
+};
