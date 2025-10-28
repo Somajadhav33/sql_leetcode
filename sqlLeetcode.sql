6bdbafd5caf530ab1e73f2bb556578429c00fab4
Somnath Jadhav <jadhavsomnath7005@gmail.com>
2025-10-28 20:21:00 +0530

    ---

--- sqlLeetcode.sql
+++ sqlLeetcode.sql
@@ -586,3 +586,26 @@ select email
 from Person
 GROUP BY email
 HAVING COUNT(email) > 1;
+
+with cte as (
+select id,visit_date,
+lag(people, 2) over (order by id) as lag2,
+lag(people, 1) over (order by id) as lag1,
+people,
+lead(people, 1) over (order by id) as lead1,
+lead(people, 2) over (order by id) as lead2
+from stadium
+)
+
+select id, visit_date, people
+from cte
+where people >=100
+and ((lead1>=100 and lead2 >=100) or (lag1>=100 and lag2 >=100) or (lag1>=100 and lead1 >=100) )
+
+
+
+
+Mere chup rehne ka sabab yeh bayaan hai,
+Naraz nahi tumse, bas dil bejaan hai,
+Jo rooth gaya, woh gulam kaisa — yeh soch aayi,
+Bas ek pal ko apni aukaat yaad aayi.
