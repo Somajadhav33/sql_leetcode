3fde802efd84c5b9ab96cfc9d3917f5ec3b1c6b9
Somnath Jadhav <jadhavsomnath7005@gmail.com>
2025-11-03 18:55:53 +0530

    OOO'

--- sqlLeetcode.sql
+++ sqlLeetcode.sql
@@ -634,5 +634,23 @@ Duniya ko batana hai tuum sirf mere ho, ab to bolo.. izazat hai?
 
 Na koi hai jise main apna keh kar fikr karu,
 Na koi jo khamoshi me meri haalat samjhe।
-Log bahut milte hain, bheed kabhi kam nahi hoti,
-Par dil ki takleef… kisi ki nazar me kabhi parhti hi nahi।
+Log bahut milte hain, bheed kabhi kam nahi huyee,
+Par dil ki takleef… kisi ki nazar me kabhi nahi aane di
+
+SELECT id,
+SUM(CASE WHEN month = 'Jan' THEN revenue END) AS Jan_Revenue,
+SUM(CASE WHEN month = 'Feb' THEN revenue END) AS Feb_Revenue,
+SUM(CASE WHEN month = 'Mar' THEN revenue END) AS Mar_Revenue,
+SUM(CASE WHEN month = 'Apr' THEN revenue END) AS Apr_Revenue,
+SUM(CASE WHEN month = 'May' THEN revenue END) AS May_Revenue,
+SUM(CASE WHEN month = 'Jun' THEN revenue END) AS Jun_Revenue,
+SUM(CASE WHEN month = 'Jul' THEN revenue END) AS Jul_Revenue,
+SUM(CASE WHEN month = 'Aug' THEN revenue END) AS Aug_Revenue,
+SUM(CASE WHEN month = 'Sep' THEN revenue END) AS Sep_Revenue,
+SUM(CASE WHEN month = 'Oct' THEN revenue END) AS Oct_Revenue,
+SUM(CASE WHEN month = 'Nov' THEN revenue END) AS Nov_Revenue,
+SUM(CASE WHEN month = 'Dec' THEN revenue END) AS Dec_Revenue
+FROM Department
+GROUP BY id;
+
+
