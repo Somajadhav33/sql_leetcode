6f92ed5460847f9ae9ab07c7e71bbb13c2f3b4a0
Somnath Jadhav <jadhavsomnath7005@gmail.com>
2025-11-08 20:43:35 +0530

    kkk

--- sqlLeetcode.sql
+++ sqlLeetcode.sql
@@ -648,7 +648,7 @@ SUM(CASE WHEN month = 'Jul' THEN revenue END) AS Jul_Revenue,
 SUM(CASE WHEN month = 'Aug' THEN revenue END) AS Aug_Revenue,
 SUM(CASE WHEN month = 'Sep' THEN revenue END) AS Sep_Revenue,
 SUM(CASE WHEN month = 'Oct' THEN revenue END) AS Oct_Revenue,
-SUM(CASE WHEN month = 'Nov' THEN revenue END) AS Nov_Revenue,
+SUM(CASE WHEN month = 'Nov' THEN revenue END) AS Nov_Reven
 SUM(CASE WHEN month = 'Dec' THEN revenue END) AS Dec_Revenue
 FROM Department
 GROUP BY id;
